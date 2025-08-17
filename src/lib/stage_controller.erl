0f1528571e7986d65d110490a10f27ca108ac44d
Fernando Areias <nando.calheirosx@gmail.com>
2025-08-17 09:42:49 -0300

    refactoring: ajuste parcial no controller

--- src/lib/stage_controller.erl
+++ src/lib/stage_controller.erl
@@ -5,11 +5,16 @@
 -record(controller_state, {
     stage_module,
     stage_pid,
-    min_workers = 1,
-    max_workers = 20,
-    scale_up_threshold = 10,  
-    scale_down_threshold = 2,
-    check_interval = 5000      % 5s
+    min_workers = 10,
+    max_workers = 200,
+    scale_up_threshold = 100,  
+    scale_down_threshold = 10,
+    check_interval = 1000,     % 1s
+    consecutive_hot = 0,
+    consecutive_cold = 0,
+    last_action_ms = 0,
+    action_cooldown_ms = 2000,
+    min_samples_to_act = 1
 }).
 
 spawn_controller(StageModule, StagePid) ->
@@ -42,25 +47,60 @@ check_stage_balance(State) ->
     
     case is_process_alive(StagePid) of
         true ->
-            {message_queue_len, QLen} = process_info(StagePid, message_queue_len),
+            MaxWorkerQLen = max_worker_queue(StageModule),
             NumWorkers = stage_behaviour:count_workers(StageModule),
             
-            io:format("[+][~p][~p] - Analisando métricas: QLen=~p, Workers=~p, Up=~p, Down=~p~n", 
-                      [calendar:local_time(), self(), QLen, NumWorkers, Up, Down]),
+            io:format("[+][~p][~p] - Métricas: MaxWorkerQLen=~p, Workers=~p, Up=~p, Down=~p~n", 
+                      [calendar:local_time(), self(), MaxWorkerQLen, NumWorkers, Up, Down]),
 
-            if
-                QLen > Up andalso NumWorkers < Max ->
-                    io:format("[+][~p][~p] - Aumento workers para ~p. QLen: ~p, Workers: ~p~n", [calendar:local_time(), self(), StageModule, QLen, NumWorkers]),
-                    stage_behaviour:add_worker(StageModule, self());
-                
-                QLen < Down andalso NumWorkers > Min ->
-                    io:format("[-][~p][~p] - Diminuindo workers para ~p. QLen: ~p, Workers: ~p~n", [calendar:local_time(), self(), StageModule, QLen, NumWorkers]),
-                    stage_behaviour:remove_worker(StageModule, any);
-                true ->
-                    io:format("[+][~p][~p] - Workers estáveis para ~p. QLen: ~p, Workers: ~p~n", [calendar:local_time(), self(), StageModule, QLen, NumWorkers])
-            end;
+            NewState1 =
+                if MaxWorkerQLen > Up -> incr_hot(reset_cold(State));
+                   MaxWorkerQLen < Down -> incr_cold(reset_hot(State));
+                   true -> reset_hot(reset_cold(State))
+                end,
+            NewState2 = maybe_scale(StageModule, NumWorkers, Min, Max, NewState1),
+            NewState2;
         false ->
             io:format("[-][~p][~p] - Stage ~p não está mais vivo~n", [calendar:local_time(), self(), StageModule])
     end,
     State.
+
+max_worker_queue(StageModule) ->
+    StageKey = list_to_atom(atom_to_list(StageModule) ++ "_workers"),
+    case ets:lookup(erlda_workers_table, StageKey) of
+        [{StageKey, Workers}] when is_list(Workers), Workers =/= [] ->
+            lists:max([queue_len(W) || W <- Workers]);
+        _ -> 0
+    end.
+
+queue_len(Pid) when is_pid(Pid) ->
+    case process_info(Pid, message_queue_len) of
+        {message_queue_len, N} -> N;
+        _ -> 0
+    end.
+
+now_ms() -> erlang:monotonic_time(millisecond).
+
+cooldown_over(#controller_state{ last_action_ms = T, action_cooldown_ms = C }) ->
+    now_ms() - T >= C.
+
+incr_hot(S = #controller_state{ consecutive_hot = H }) -> S#controller_state{ consecutive_hot = H + 1 }.
+incr_cold(S = #controller_state{ consecutive_cold = C }) -> S#controller_state{ consecutive_cold = C + 1 }.
+reset_hot(S) -> S#controller_state{ consecutive_hot = 0 }.
+reset_cold(S) -> S#controller_state{ consecutive_cold = 0 }.
+
+maybe_scale(StageModule, NumWorkers, Min, Max, S = #controller_state{ consecutive_hot = H, consecutive_cold = C, min_samples_to_act = K }) ->
+    CanAct = cooldown_over(S),
+    case true of
+        _ when H >= K andalso NumWorkers < Max andalso CanAct ->
+            io:format("[+][~p][~p] - Scale UP ~p (consecutive_hot=~p)~n", [calendar:local_time(), self(), StageModule, H]),
+            stage_behaviour:add_worker(StageModule),
+            S#controller_state{ consecutive_hot = 0, last_action_ms = now_ms() };
+        _ when C >= K andalso NumWorkers > Min andalso CanAct ->
+            io:format("[-][~p][~p] - Scale DOWN ~p (consecutive_cold=~p)~n", [calendar:local_time(), self(), StageModule, C]),
+            stage_behaviour:remove_worker(StageModule, any),
+            S#controller_state{ consecutive_cold = 0, last_action_ms = now_ms() };
+        _ ->
+            S
+    end.
  
--- src/lib/worker.erl
+++ src/lib/worker.erl
@@ -9,10 +9,12 @@ loop(StageModule) ->
     receive
         {work, Command, From} ->
             case StageModule:handle_command(Command) of
-                {ok, _} ->
+                {ok, Result} ->
+                    From ! {stage_result, Result},
                     ok;
                 {error, Reason} ->
-                    io:format("[-][~p][~p] - Erro motivo ~p ~n", [calendar:local_time(), self(), Reason]);
+                    io:format("[-][~p][~p] - Erro motivo ~p ~n", [calendar:local_time(), self(), Reason]),
+                    From ! {stage_error, Reason};
                 {forward, NextStagePid, NewCommand} ->
                     NextStagePid ! {command, NewCommand, From}
             end,
